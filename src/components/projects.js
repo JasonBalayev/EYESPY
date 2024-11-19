6aaed1841bce8543122785e6d3366c039e4feed1
Jason Balayev <156387759+JasonBalayev@users.noreply.github.com>
2024-11-19 00:44:30 -0500

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,6 @@
 export var projects = [
   {
-    name: "jasonbalayev.dev 🔧 (Public - Click here for github)",
+    name: "Portfolio 🔧 (Click here for github)",
     description:
       "jasonbalayev.dev is a portfolio website that I created using React.js showcasing my skills within front-end, back-end development, and general programming skills through projects. This website was built using Gatsby.js, Javascript, and Tailwind CSS, and hosted on Netlify for optimal performance.",
     badges: ["React,js", "Gatsby.js", "Three.js", "Tailwind CSS", "Javascript", "Git/Github"],
@@ -16,14 +16,14 @@ export var projects = [
   },
 
   {
-    name: "Resume LaTeX Generator 📝 (Public - Click here for github)",
+    name: "Resume LaTeX Generator 📝 (Click here for github)",
     description:
       "(ONGOING) A collaborative project for the Northeastern Oasis Project Series team. This project focuses on software development for standard resumes converted into a LaTeX document.",
     badges: ["React.js", "OpenAI API", "TypeScript", "Git/Github"],
     link: "https://github.com/Oasis-NEU/f24-group17",
   },
   {
-    name: "Jason's AI Space 🤖 (Public - Click here for github)",
+    name: "Jason's AI Space 🤖 (Click here for github)",
     description:
       "Jason's AI Space is a virtual assistant modeled as an AI version of me, designed to answer any question using an advanced LLM. This project is used as a free space where users can engage with Jason's digital twin to gain insights, learn, and explore a variety of topics.",
     badges: ["Next.js", "Convex", "JavaScript", "OpenAI API", "Git/Github"],
@@ -31,14 +31,14 @@ export var projects = [
     liveLink: "https://jasonsai-space.vercel.app/",
   },
   {
-    name: "Northeastern Electric Racing 🏎️ (Public - Click here for github)",
+    name: "Northeastern Electric Racing 🏎️ (Click here for github)",
     description:
       "(ONGOING) A collaborative project contributing to the Northeastern Electric Racing team. This project focuses on software development for electric race car systems, including telemetry, data logging, and optimization algorithms.",
     badges: ["Express.js", "React.js", "TypeScript", "Prisma", "Docker", "Git/Github"],
     link: "https://github.com/JasonBalayev/Northeastern-Electric-Racing",
   },
   {
-    name: "AI-Convex 🤖 (Public - Click here for github)",
+    name: "AI-Convex 🤖 (Click here for github)",
     description:
       "AI-Convex is an AI-powered To-Do list application. This project integrates AI to enhance task management efficiency, offering personalized suggestions and smart features.",
     badges: ["Next.js", "Convex", "TypeScript", "Clerk Auth", "OpenAI API", "Git/Github"],
@@ -46,14 +46,14 @@ export var projects = [
     liveLink: "https://ai-convex-mu.vercel.app/",
   },
   {
-    name: "LeetCode Solutions (Public - Click here for github)",
+    name: "LeetCode Solutions (Click here for github)",
     description:
       "This repository stores all of my solutions and progress on different LeetCode problems.",
     badges: ["LeetCode", "Java", "Python", "Git/Github"],
     link: "https://github.com/JasonBalayev/LeetCodeSolutions",
   },
   {
-    name: "Monkeypox Visualizer 🦠 (Public - Click here for github)",
+    name: "Monkeypox Visualizer 🦠 (Click here for github)",
     description:
       "(ONGOING) MPOX is an advanced project focused on exploring the impact of viral outbreaks and epidemic modeling using Python. It includes the use of machine learning techniques to predict infection spread based on various parameters.",
     badges: ["Python", "Django", "OpenAI API", "Postman", "Three.js", "Git/Github"],
@@ -61,7 +61,7 @@ export var projects = [
   },
 
   {
-    name: "AI SaaS FlashSet 🤖 (Public - Click here for github)",
+    name: "AI SaaS FlashSet 🤖 (Click here for github)",
     description:
       "AI SaaS Flashcard is an AI-powered software as a service (SaaS) application designed for effortless flashcard creation and management given any prompt by the user. Basic and pro features are currently in test mode. Although the app is a work in progress, it aims to provide an educational experience for learners.",
     badges: ["Next.js", "Firebase", "JavaScript", "Clerk Auth", "Stripe/OpenAI API", "Git/Github"],
@@ -76,7 +76,7 @@ export var projects = [
     link: "https://github.com/JasonBalayev/Android-Application",
   },
   {
-    name: "Minimax Algorithm Through Tic-Tac-Toe (Public - Click here for github)",
+    name: "Minimax Algorithm Through Tic-Tac-Toe (Click here for github)",
     description:
       "A simple Tic-Tac-Toe game built using Python, HTML, CSS, and JavaScript. This project showcases basic game logic implementation and interactive UI design for two players. Using JavaScript to incorporate the minimax algorithm to win at all times in a standard 3x3 match, and use the best possible moves in a 4x4 and 5x5.",
     badges: ["Python", "HTML", "CSS", "JavaScript", "Django", "Git/Github"],
