83c3d1f46eebde18451c480e1b5d553ecafb33cf
Jason <jasonbalayev@gmail.com>
2024-11-18 20:31:07 -0500

    Black Background

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -6,16 +6,18 @@ import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
-    <Layout>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className="relative z-0 text-white text-arial"
-      >
-        <ProjectCards projects={projects} />
-      </motion.div>
-    </Layout>
+    <div className="bg-black min-h-screen">
+      <Layout>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="relative z-0 text-white text-arial"
+        >
+          <ProjectCards projects={projects} />
+        </motion.div>
+      </Layout>
+    </div>
   );
 };
 
